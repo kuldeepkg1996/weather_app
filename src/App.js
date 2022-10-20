9686f19f3961fe77c4a1f0c549ac191c334b348f
Abhishek Kumar Gupta <abgupta@altimetrik.com>
2022-10-20 08:38:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
-
+import axios from 'axios'
+import ShowTemp from './ShowTemp'
 function App() {
+  const [city,setCity]=useState("")
+  const [data,setData]=useState({
+    Date:"",
+    Time:"",
+    Temp:"",
+    WindDirection:"",
+    WindSpeed:"",
+    WeatherDescription:"",
+    Humidity:""
+
+  })
+
+
+
+  const handleClick=()=>{
+    axios.get(`http://api.weatherstack.com/current?access_key=38d85564cc1ff0bc9ccfa5585c72291e&query= ${city}`)
+    .then((response)=>{
+      console.log(response.data);
+      setData({
+        Date:response.data?.location?.localtime.slice(0, 9),
+        Time:response.data?.location?.localtime.slice(10),
+        Temp:response.data.current.temperature,
+        WindDirection:response.data.current.wind_dir,
+        WindSpeed:response.data.current.wind_speed,
+        WeatherDescription:response.data.current.weather_descriptions[0],
+        Humidity:response.data.current.humidity
+      })
+    })
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+     
+     <div className='App'>
+
+    <div className='container text-center my-5'>
+    <h1>Weeather App using React Js</h1>
+    <input type="text" className=' my-5 search' value={city} onChange={(e)=>{
+      setCity(e.target.value);
+    }}/>
+    <button className='btn btn-primary mx-2 ' type='submit' onClick={handleClick}>Get Weather</button>
+
     </div>
+    <div>
+
+     <ShowTemp text={data}></ShowTemp>
+
+     </div>
+
+
+  
+     </div>
+    
+
   );
 }
 
